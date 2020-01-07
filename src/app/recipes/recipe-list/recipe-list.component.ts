c30c855d6ba5a6bb9a7fbf2d99a0f874b7298bbd
Vaishak Murali <vaishakmurali@gmail.com>
2020-01-06 18:06:06 -0800

    Code update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Chicken Tikka Masala', 'A popular north Indian chicken dish.',
+    'https://s3-media0.fl.yelpcdn.com/bphoto/NtaiOC1qC7WMuBhBnYhGkg/o.jpg'),
+    new Recipe('Chicken Vindaloo', 'South Indian chicken curry with potatoes.',
+    'https://s3-media0.fl.yelpcdn.com/bphoto/cApB3Pj8Xr4kSRaCjUCrEw/o.jpg'),
+    new Recipe('Chole  Masala', 'Punjabi style chickpea dish.',
+    'https://s3-media0.fl.yelpcdn.com/bphoto/Xzql6vakNrollYvxRnOgDQ/o.jpg'),
+    new Recipe('Saag Paneer', 'Spinach and cottage cheese dish.',
+    'https://s3-media0.fl.yelpcdn.com/bphoto/NtaiOC1qC7WMuBhBnYhGkg/o.jpg')
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor() {}
+  ngOnInit() {}
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', '1lb'),
+    new Ingredient('Potatoes', '1lb'),
+  ];
+
+
 
   constructor() { }
 
